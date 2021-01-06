10932debe289667fbd57b1f9031efbcfc67c8b79
Daniel Derkacz <mail@daniel-derkacz.de>
2021-01-06 16:48:01 +0100

    updated implementation of possibility check

--- grid.py
+++ grid.py
@@ -36,6 +36,26 @@ class Grid:
         row_end = int(row + cell_size)
         column = int(cell_size * column_offset)
         column_end = int(column + cell_size)
-        print(
-            f"row {row} row_end { row_end } column {column} column_end { column_end }")
         return self.grid[row:row_end, column:column_end]
+
+    def sum_of_values(self):
+        # small gauss
+        return (self._width ** 2 + self._width) / 2
+
+    def is_possible_in_row(self, row_id, number):
+        return 0 in self.row(row_id) and not(number in self.row(row_id))
+
+    def is_possible_in_column(self, column_id, number):
+        return 0 in self.column(column_id) and not(number in self.column(column_id))
+
+    def is_possible_in_cell(self, cell_id, number):
+        return 0 in self.cell(cell_id).flatten() and not(number in self.cell(cell_id).flatten())
+
+    def coordinate_to_cell(self, row, column):
+        cell_size = int(np.sqrt(self._width))
+        row_offset, column_offset = int(
+            row//cell_size), int(column % cell_size)
+        return row_offset * cell_size + column_offset
+
+    def is_possible(self, row, column, number):
+        return self.is_possible_in_row(row, number) and self.is_possible_in_column(column, number) and self.is_possible_in_cell(self.coordinate_to_cell(row, column), number)
--- grid_tests.py
+++ grid_tests.py
@@ -16,46 +16,70 @@ class TestGridLoading(unittest.TestCase):
         [1, 2, 3, 4, 5, 6, 7, 8, 9],
     ])
 
+    grid = Grid(test_grid)
+
     def test_from_file(self):
         grid = Grid.from_file("test.board")
         target = self.test_grid
         self.assertTrue(np.array_equal(grid.grid, target))
 
     def test_select_row_one(self):
-        grid = Grid(self.test_grid)
-        row = grid.row(0)
+        row = self.grid.row(0)
         target = [1, 3, 3, 4, 5, 6, 7, 8, 9]
         self.assertTrue(np.array_equal(row, target))
 
     def test_select_row_seven(self):
-        grid = Grid(self.test_grid)
-        row = grid.row(6)
+        row = self.grid.row(6)
         target = [1, 2, 3, 4, 5, 6, 7, 8, 0]
         self.assertTrue(np.array_equal(row, target))
 
     def test_select_column_one(self):
-        grid = Grid(self.test_grid)
-        column = grid.column(0)
+        column = self.grid.column(0)
         target = [1, 0, 1, 1, 1, 1, 1, 1, 1]
         self.assertTrue(np.array_equal(column, target))
 
     def test_select_column_four(self):
-        grid = Grid(self.test_grid)
-        column = grid.column(3)
+        column = self.grid.column(3)
         target = [4, 0, 9, 4, 4, 4, 4, 4, 4]
         self.assertTrue(np.array_equal(column, target))
 
     def test_select_cell_one(self):
-        grid = Grid(self.test_grid)
-        cell = grid.cell(0).flatten()
+        cell = self.grid.cell(0).flatten()
         target = [1, 3, 3, 0, 0, 0, 1, 2, 3]
         self.assertTrue(np.array_equal(cell, target))
 
     def test_select_cell_five(self):
-        grid = Grid(self.test_grid)
-        cell = grid.cell(4).flatten()
+        cell = self.grid.cell(4).flatten()
         target = [4, 5, 6, 4, 5, 6, 4, 5, 6]
         self.assertTrue(np.array_equal(cell, target))
+
+    def test_is_two_possible_in_row_two(self):
+        result = self.grid.is_possible_in_row(1, 2)
+        self.assertTrue(result)
+
+    def test_is_two_possible_in_row_three(self):
+        result = self.grid.is_possible_in_row(2, 2)
+        self.assertFalse(result)
+
+    def test_is_two_possible_in_column_nine(self):
+        result = self.grid.is_possible_in_column(8, 2)
+        self.assertTrue(result)
+
+    def test_is_nine_possible_in_column_nine(self):
+        result = self.grid.is_possible_in_column(8, 9)
+        self.assertFalse(result)
+
+    def test_is_nine_possible_in_cell_two(self):
+        result = self.grid.is_possible_in_cell(1, 9)
+        self.assertFalse(result)
+
+    def test_is_nine_possible_in_cell_one(self):
+        result = self.grid.is_possible_in_cell(0, 9)
+        self.assertTrue(result)
+
+    def test_is_nine_possible_in_cell_seven(self):
+        result = self.grid.is_possible_in_cell(6, 9)
+        self.assertFalse(result)
 
 
 if __name__ == "__main__":
