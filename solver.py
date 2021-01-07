e5a6de2d18c588501c1531852703a0e2825c8dce
Daniel Derkacz <mail@daniel-derkacz.de>
2021-01-07 19:50:56 +0100

    random sample a value

--- solver.py
+++ solver.py
@@ -1,4 +1,5 @@
 from blacklist import Blacklist
+import numpy as np
 
 
 class BackwardSolver():
@@ -11,12 +12,8 @@ class BackwardSolver():
         self.blacklist = Blacklist(self.board.width)
 
     def step(self):
-        possible_values = self.board.possible_values(
-            self.current_row, self.current_column)
         is_fixed_value = self.board.is_fixed_value(
             self.current_row, self.current_column)
-        reduced_list = self.blacklist.reduce(
-            self.current_row, self.current_column, possible_values)
         # got a fixed value and just increment the position and
         if is_fixed_value:
             print(
@@ -25,7 +22,8 @@ class BackwardSolver():
             self.check_for_solved()
             return
 
-        if reduced_list == []:
+        sampled_value = self.sample_value()
+        if sampled_value is None:
             # backtrack
             print(
                 f"this is not correct. at position {self.current_row}, {self.current_column}")
@@ -48,19 +46,28 @@ class BackwardSolver():
 
         else:
             # take first possible value
-            print(f"possible values {reduced_list}")
             print(
                 f"on blacklist {self.blacklist.get_entry(self.current_row, self.current_column)}")
             print(
-                f"setting value {reduced_list[0]} at {self.current_row}, {self.current_column}")
+                f"setting value {sampled_value} at {self.current_row}, {self.current_column}")
             self.board.set(self.current_row,
-                           self.current_column, reduced_list[0])
+                           self.current_column, sampled_value)
             self.increment_position()
             self.check_for_solved()
 
     def solve(self):
         while not self.solved and not self.stop:
             self.step()
+
+    def sample_value(self):
+        possible_values = self.board.possible_values(
+            self.current_row, self.current_column)
+        reduced_list = self.blacklist.reduce(
+            self.current_row, self.current_column, possible_values)
+        print(f"possible values {reduced_list}")
+        if reduced_list == []:
+            return None
+        return np.random.choice(reduced_list)
 
     def increment_position(self):
         if self.current_column + 1 >= self.board.width:
