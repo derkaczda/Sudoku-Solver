e097985adc76716716d4e7eaba2a9b15a3d6f6d6
Daniel Derkacz <mail@daniel-derkacz.de>
2021-01-08 10:58:52 +0100

    clean up

--- main.py
+++ main.py
@@ -7,19 +7,9 @@ if __name__ == '__main__':
     parser.add_argument('--file', default="", type=str,
                         help='path to a board file')
 
-    parser.add_argument('--steps', default=1000,
-                        type=int, help='number of steps')
     arguments = parser.parse_args()
     if arguments.file != "":
         grid = Grid.from_file(arguments.file)
         solver = BackwardSolver(grid)
-#        solver.solve()
-#        print(grid.grid)
-        solver.debug = False
         solver.solve()
         grid.show()
-#        while not solver.solved:
-#            solver.step()
-#            grid.show()
-#            a = input()
-#        print("solved")
--- solver.py
+++ solver.py
@@ -6,28 +6,17 @@ class BackwardSolver():
     def __init__(self, board):
         self.board = board
         self.current_row = 0
-        self.last_correct_row = 0
         self.current_column = 0
-        self.last_correct_column = 0
         self.solved = False
-        self.stop = False
         self.blacklist = Blacklist(self.board.width)
         self.step_backward = False
-        self.row_to_solve = -1
-        self.column_to_solve = -1
-        self.max_row = -1
-        self.max_column = -1
         self.max_index = -1
         self.set_next_possible_position()
-        self.debug = False
 
     def step(self):
         sampled_value = self.sample_value()
         # best step just use sampled value
         if sampled_value is not None:
-            if self.debug:
-                print(
-                    f"setting {sampled_value} at position {self.current_row}, {self.current_column}")
             self.board.set(self.current_row,
                            self.current_column, sampled_value)
             if self.board.solved():
@@ -47,9 +36,6 @@ class BackwardSolver():
             possible_values = self.blacklist.reduce(
                 self.current_row, self.current_column, possible_values)
             while possible_values == []:
-                if self.debug:
-                    print(
-                        f"after blacklist no possible values at {self.current_row}, {self.current_column}")
                 self.blacklist.clear(self.current_row, self.current_column)
                 self.set_next_possible_position(backward=True)
                 wrong_value = self.board.pop(
@@ -83,24 +69,15 @@ class BackwardSolver():
 
     def set_new_max_position(self):
         self.max_index = self.current_row * self.board.width + self.current_column
-        self.max_row = self.current_row
-        self.max_column = self.current_column
 
     def sample_value(self):
         possible_values = self.board.possible_values(
             self.current_row, self.current_column)
         reduced_list = self.blacklist.reduce(
             self.current_row, self.current_column, possible_values)
-        if self.debug:
-            print(f"possible values {reduced_list}")
         if reduced_list == []:
             return None
         return np.random.choice(reduced_list)
-
-    def reduce(self, possible_values):
-        intersection = np.intersect1d(
-            np.array(self.blacklist), np.array(possible_values))
-        return [number for number in possible_values if number not in intersection]
 
     def increment_position(self):
         if self.current_column + 1 >= self.board.width and self.current_row + 1 >= self.board.width:
@@ -122,7 +99,3 @@ class BackwardSolver():
             self.current_column -= 1
             if self.current_column < 0:
                 self.current_column = 0
-
-    def check_for_solved(self):
-        if self.current_row == self.board.width-1 and self.current_column == self.board.width-1:
-            self.solved = True
