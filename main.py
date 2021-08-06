67bf56a293664bdf2f187033c9278568aca39bd5
Daniel Derkacz <mail@daniel-derkacz.de>
2021-08-06 16:24:43 +0200

    Draw 9x9 Sudoku grid

--- main.py
+++ main.py
@@ -19,14 +19,21 @@ if __name__ == '__main__':
 
 
     pygame.init()
-
+    
+    full_red = (255, 0, 0)
+    full_green = (0, 255, 0)
+    full_blue = (0, 0, 255)
+    black = (50, 50, 50)
     # Background color
     BACKGROUND_COLOR = (255, 255, 255)
 
     FPS = 60
     fpsClock = pygame.time.Clock()
-    WINDOW_WIDTH = 400
-    WINDOW_HEIGHT = 300
+    WINDOW_WIDTH = 600
+    WINDOW_HEIGHT = 600
+    grid_size = 9
+    width_delta = WINDOW_WIDTH / grid_size
+    height_delta = WINDOW_HEIGHT / grid_size
 
     window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
     pygame.display.set_caption('Sudoku solver')
@@ -42,6 +49,15 @@ if __name__ == '__main__':
             # Process stuff here
 
             window.fill(BACKGROUND_COLOR)
+            
+            line_thickness = 1
+
+            for i in range(1, grid_size + 1):
+                new_y = int(i * height_delta)
+                new_x = int(i * width_delta)
+                pygame.draw.line(window, black, (0, new_y), (WINDOW_WIDTH, new_y), line_thickness)
+                pygame.draw.line(window, black, (new_x, 0), (new_x, WINDOW_HEIGHT), line_thickness)
+
             pygame.display.update()
             fpsClock.tick(FPS)
 
