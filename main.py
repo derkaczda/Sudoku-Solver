7c62fd99f7f2c24b7bbe652c46cb01f8c72c9c08
Daniel Derkacz <mail@daniel-derkacz.de>
2021-08-06 16:06:52 +0200

    Simple pygame window

--- main.py
+++ main.py
@@ -2,6 +2,9 @@ import argparse
 from solver.solver import BackwardSolver
 from solver.grid import Grid
 
+import pygame, sys, random
+from pygame.locals import *
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--file', default="", type=str,
@@ -13,3 +16,33 @@ if __name__ == '__main__':
         solver = BackwardSolver(grid)
         solver.solve()
         grid.show()
+
+
+    pygame.init()
+
+    # Background color
+    BACKGROUND_COLOR = (255, 255, 255)
+
+    FPS = 60
+    fpsClock = pygame.time.Clock()
+    WINDOW_WIDTH = 400
+    WINDOW_HEIGHT = 300
+
+    window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+    pygame.display.set_caption('Sudoku solver')
+
+    def main():
+        looping = True
+        while looping:
+            for event in pygame.event.get():
+                if event.type == QUIT:
+                    pygame.quit()
+                    sys.exit()
+
+            # Process stuff here
+
+            window.fill(BACKGROUND_COLOR)
+            pygame.display.update()
+            fpsClock.tick(FPS)
+
+    main()
